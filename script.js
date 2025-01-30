831da0f54fff8ddc37fd0c985ce7b6abf460bf9b
MFQ09 <mdfaizanqureshi45@gmail.com>
2025-01-30 19:20:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+// Animation for sections on scroll
 const sections = document.querySelectorAll('.section');
 
 const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('animate');
-      observer.unobserve(entry.target);
-    }
-  });
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+            observer.unobserve(entry.target);
+        }
+    });
 }, { threshold: 0.5 });
 
 sections.forEach(section => {
-  observer.observe(section);
+    observer.observe(section);
 });
