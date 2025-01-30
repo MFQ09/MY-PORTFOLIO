dee604bc7203d6239b395f18e6194a2902c2a05d
MFQ09 <mdfaizanqureshi45@gmail.com>
2025-01-30 19:15:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.5 });
+
+sections.forEach(section => {
+  observer.observe(section);
+});
